515fd6133ec0620daaf702e24d2422591d626b73
maruki00 <aitbaha48@gmail.com>
2024-08-14 20:14:23 +0300

    --

---
+++ internal/dbhandler.go
@@ -0,0 +1,28 @@
+package internal
+
+import (
+	"database/sql"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var db *sql.DB
+
+func GetDB() *sql.DB {
+
+	if db != nil {
+		return db
+	}
+	db, err := sql.Open("mysql", "user:password@/dbname")
+	if err != nil {
+		panic(err)
+	}
+	// See "Important settings" section.
+	db.SetConnMaxLifetime(time.Minute * 3)
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(10)
+
+	return db
+
+}
